1e3dc2e427e00332863cd6c57ded74c4098c2ceb
jhaunn <89385585+jhaunn@users.noreply.github.com>
2022-10-11 15:43:54 +0800

    Revert "Added all projects to GitHub"
    This reverts commit 2ae0d6c7416a4704c6c5a15934cb6de6ab1290d2.

--- Day 4 - Hidden Search Widget/app.js
+++
@@ -1,8 +0,0 @@
-let searchButton = document.querySelector("#search-button");
-let searchBar = document.querySelector("#search-bar");
-
-searchButton.addEventListener('click', function() {
-   searchBar.classList.add("show-search");
-   searchBar.classList.remove("hide-search");
-   searchBar.focus();
-});
