b6c640150190664b0eca085be910436af7c12bd2
jhaunn <89385585+jhaunn@users.noreply.github.com>
2022-11-11 12:33:04 +0800

    Update Day 29

--- Day 29 - Double Heart Click/app.js
+++ Day 29 - Double Heart Click/app.js
@@ -4,6 +4,24 @@ let counter = 0;
 
 photo.addEventListener('dblclick', (e) => {
    counter++;
+
+   const x = e.clientX;
+   const y = e.clientY;
+
+   const leftOffset = e.target.offsetLeft;
+   const topOffset = e.target.offsetTop;
+
+   const xInside = x - leftOffset;
+   const yInside = y - topOffset;
+
+   const heart = document.createElement('i');
+   heart.classList.add('fa-solid');
+   heart.classList.add('fa-heart');
+   heart.classList.add('heart');
+
+   heart.style.top = `${yInside}px`;
+   heart.style.left = `${xInside}px`;
    
    counterEl.innerHTML = `You liked it ${counter} times`;
+   photo.appendChild(heart);
 });
