db2d83aa2ea619d574c3e0c34e6d9510ce019cd7
jhaunn <89385585+jhaunn@users.noreply.github.com>
2022-11-18 16:41:17 +0800

    Update Day 33

--- Day 33 - Notes App/app.js
+++ Day 33 - Notes App/app.js
@@ -2,12 +2,33 @@ let addBtn = document.querySelector('button');
 let board = document.querySelector('#board');
 
 addBtn.addEventListener('click', e => {
-   board.innerHTML += 
-      `<div class="note">
-         <header>
-            <i class="fa-solid fa-pen-to-square"></i>
-            <i class="fa-solid fa-trash"></i>
-         </header>
-         <textarea></textarea>
-      </div>`;
+   let note = document.createElement('div');
+   let textarea = document.createElement('textarea');
+   note.classList.add('note');
+   note.innerHTML = 
+      `<header>
+         <i class="fa-solid fa-pen-to-square edit"></i>
+         <i class="fa-solid fa-trash trash"></i>
+      </header>`;
+   note.appendChild(textarea);
+   board.appendChild(note);
+
+   addNoteFuncs();
 });
+
+function addNoteFuncs() {
+   let editBtn = document.querySelectorAll('.edit');
+   let trashBtn = document.querySelectorAll('.trash');
+
+   editBtn.forEach(element => {
+      element.addEventListener('click', e => {
+         element.parentElement.parentElement.querySelector('textarea').value = 'Edited';
+      });
+   });
+
+   trashBtn.forEach(element => {
+      element.addEventListener('click', e => {
+         element.parentElement.parentElement.remove();
+      });
+   });
+}
