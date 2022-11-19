25725fdb8874e08a8d95f33d69bb6cffae1e47cf
jhaunn <89385585+jhaunn@users.noreply.github.com>
2022-11-19 14:25:59 +0800

    Update app.js

--- Day 33 - Notes App/app.js
+++ Day 33 - Notes App/app.js
@@ -1,7 +1,18 @@
 let addBtn = document.querySelector('button');
 let board = document.querySelector('#board');
+let notes = JSON.parse(localStorage.getItem('notes'));
+
+if (notes) {
+   notes.forEach(element => {
+      addNotes(element);
+   });
+}
 
 addBtn.addEventListener('click', e => {
+   addNotes('');
+});
+
+function addNotes(content) {
    let note = document.createElement('div');
    let textarea = document.createElement('textarea');
    note.classList.add('note');
@@ -13,22 +24,45 @@ addBtn.addEventListener('click', e => {
    note.appendChild(textarea);
    board.appendChild(note);
 
+   textarea.addEventListener('input', e => {
+      updateStorage();
+   });
+
+   textarea.value = content;
+
+   updateStorage();
+
    addNoteFuncs();
-});
+}
 
 function addNoteFuncs() {
+   let readonly = false;
    let editBtn = document.querySelectorAll('.edit');
    let trashBtn = document.querySelectorAll('.trash');
 
    editBtn.forEach(element => {
       element.addEventListener('click', e => {
-         element.parentElement.parentElement.querySelector('textarea').value = 'Edited';
+         element.parentElement.parentElement.querySelector('textarea').readOnly = !element.parentElement.parentElement.querySelector('textarea').readOnly;
+         updateStorage();
       });
    });
 
    trashBtn.forEach(element => {
       element.addEventListener('click', e => {
          element.parentElement.parentElement.remove();
+         updateStorage();
       });
    });
+}
+
+function updateStorage() {
+   let textareas = document.querySelectorAll('textarea');
+
+   let notes = [];
+
+   textareas.forEach(element => {
+      notes.push(element.value)
+   });
+
+   localStorage.setItem('notes', JSON.stringify(notes));
 }
