51de6a3626bf5b655179e7a395246015ec860ae6
jhaunn <89385585+jhaunn@users.noreply.github.com>
2022-11-24 18:09:36 +0800

    Update app.js

--- Day 34 - Animated Countdown/app.js
+++ Day 34 - Animated Countdown/app.js
@@ -5,22 +5,6 @@ let replayBtn = document.querySelector('#replay button');
 
 let current = 3;
 
-// let startCountdown = setInterval(function() {
-//    if (current === 0) {
-//       replayEl.classList.remove('hidden');
-//       countdownEl.classList.remove('active');
-//       replayEl.classList.add('active');
-//       countdownEl.classList.add('hidden');
-
-//       current = 3;
-
-//       clearInterval(startCountdown);
-//    }
-
-//    current -= 1;
-//    countdownText.innerHTML = current.toString();
-// }, 1000);
-
 let startCountdown = setInterval(function() {
    if (current === 0) {
       replayEl.classList.remove('hidden');
