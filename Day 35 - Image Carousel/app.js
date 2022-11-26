471ff57f187365db30f8cbe63f30cfc2f68f148f
jhaunn <89385585+jhaunn@users.noreply.github.com>
2022-11-26 20:06:23 +0800

    Update Day 35

---
+++ Day 35 - Image Carousel/app.js
@@ -0,0 +1,18 @@
+let carousel = document.querySelector('#carousel'); 
+let prevBtn = document.querySelector('#prev');
+let nextBtn = document.querySelector('#next');
+
+// nextBtn.addEventListener('click', e => {
+//    document.querySelectorAll('#carousel img').forEach(element => {
+//       element.classList.remove('active');
+//       element.classList.add('inactive');
+//    });
+
+//    let img = document.createElement('img');
+//    img.src = './Images/wallpaperflare.com_wallpaper.jpg';
+//    carousel.appendChild(img);
+
+//    setTimeout(() => {
+//       img.classList.add('active');
+//    }, 10)
+// });
